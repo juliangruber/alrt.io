29bf20985273cee0d70bdfd3c5aa8d7a8b592fb2
Julian Gruber <julian@juliangruber.com>
2012-08-17 08:51:40 +0200

    quite finished

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,41 @@
  * GET home page.
  */
 
-var SECOND = 1000;
-var MINUTE = SECOND*60;
-var HOUR = MINUTE*60;
-var DAY = HOUR*24;
-var WEEK = DAY*7;
+var intervals = {
+ WEEK: 604800000,
+ DAY: 86400000,
+ HOUR: 3600000,
+ MINUTE: 60000,
+ SECOND: 1000
+}
 
 exports.index = function(req, res) {
+  var date = parseDate(req.params.time);
   res.render('index', {
     title: 'Alrt',
-    duration: getMiliseconds(parseDate(req.params.time))
+    duration: getMiliseconds(date),
+    largest: getLargest(date)
   });
 };
+
+function getLargest(date) {
+  for (type in date) {
+    if (type == 'w') return intervals.WEEK;
+    if (type == 'd') return intervals.DAY;
+    if (type == 'h') return intervals.HOUR;
+    if (type == 'm') return intervals.MINUTE;
+    if (type == 's') return intervals.SECOND;
+  } 
+}
 
 function getMiliseconds(date) {
  var ms = 0;
  for (type in date) {
-   if (type == 's') ms += date[type] * SECOND;
-   if (type == 'm') ms += date[type] * MINUTE;
-   if (type == 'h') ms += date[type] * HOUR;
-   if (type == 'd') ms += date[type] * DAY;
-   if (type == 'w') ms += date[type] * WEEK;
+   if (type == 's') ms += date[type] * intervals.SECOND;
+   if (type == 'm') ms += date[type] * intervals.MINUTE;
+   if (type == 'h') ms += date[type] * intervals.HOUR;
+   if (type == 'd') ms += date[type] * intervals.DAY;
+   if (type == 'w') ms += date[type] * intervals.WEEK;
  }
  return ms;
 }
