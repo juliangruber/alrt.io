00fd4f4059af3b0e3b5c66ea1d3020a7f2a39d48
Julian Gruber <julian@juliangruber.com>
2013-12-13 10:05:40 -0800

    refactor app.js

--- app.js
+++ app.js
@@ -5,21 +5,16 @@
 
 var express = require('express');
 var routes = require('./routes');
-var path = require('path');
 var app = module.exports = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.static(__dirname + '/public'))
-  app.use(app.router);
-});
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.static(__dirname + '/public'))
 
-app.configure('development', function(){
+if (process.env.NODE_ENV != 'production') {
   app.use(express.errorHandler());
-});
+}
 
 app.get('/', routes.help);
 app.get('/:time', routes.index);
